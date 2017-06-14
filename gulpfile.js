4fc2a3a158db961998e50ae6630f36a9b5959f89
Gerben <gerbengeeraerts@gmail.com>
2017-06-14 21:13:26 +0200

    Init of gulpfile.js

---
+++ gulpfile.js
@@ -0,0 +1,22 @@
+var gulp = require('gulp'),
+		compass = require('gulp-compass'),
+		gutil = require('gulp-util'),
+
+gulp.task('styles', function(){
+	return gulp.src('./css/**/*.scss')
+		.pipe(compass({
+			css: './deploy/css',
+			sass: './css',
+			environment: 'production'
+		}))
+		.on('error', function(err){
+			gutil.log(err.message);
+			gutil.beep();
+			this.emit('end');
+		})
+		.pipe(gulp.dest('./deploy/css'));
+});
+
+gulp.task('watch', ['scripts', 'styles'], function(){
+	gulp.watch(['css/**/*.scss'], ['styles']);
+});
